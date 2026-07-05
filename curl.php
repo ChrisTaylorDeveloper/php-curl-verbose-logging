253867ccb585ae89c67a672e2eb7431aacc03c48
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-07-05 17:52:06 +0100

    Minimal working example.

---
+++ curl.php
@@ -0,0 +1,24 @@
+<?php
+
+// $logFile = __DIR__ . '/curl.log';
+
+$fp = fopen(__DIR__ . '/curl.log', 'a');
+
+$postData = [
+    'username' => 'john_doe',
+    'email'    => 'john@example.com'
+];
+
+$ch = curl_init('https://httpbin.org/post');
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+curl_setopt($ch, CURLOPT_STDERR, $fp);
+
+curl_exec($ch);
+
+curl_close($ch);
+fclose($fp);
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+rm -f curl.log
+touch curl.log
+php curl.php
